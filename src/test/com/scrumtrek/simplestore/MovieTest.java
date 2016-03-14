da642ab95382094bc143da55fc06b5685a5dd60d
SuperDuperPugach <pugachevdmitry67@gmail.com>
2016-03-14 13:06:48 +0400

    test

---
+++ src/test/com/scrumtrek/simplestore/MovieTest.java
@@ -0,0 +1,27 @@
+package com.scrumtrek.simplestore;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by pugach on 14/03/16.
+ */
+public class MovieTest {
+
+    @Test
+    public void getPriceCodeMethodTest() {
+        Movie movie = new Movie("BNL", PriceCodes.NewRelease);
+
+        assertEquals(movie.getPriceCode(), PriceCodes.NewRelease);
+    }
+
+    @Test
+    public void setPriceCodeMethodTest() {
+        Movie movie = new Movie("BNL", PriceCodes.NewRelease);
+        movie.setPriceCode(PriceCodes.Childrens);
+
+        assertEquals(movie.getPriceCode(), PriceCodes.Childrens);
+    }
+
+}
