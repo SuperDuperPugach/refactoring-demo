16da2cdcf4aea2a2ad048e0abfc6fb1f8fb44959
SuperDuperPugach <pugachevdmitry67@gmail.com>
2016-03-14 15:05:20 +0400

    тест для класса Customer

--- src/test/java/com/scrumtrek/simplestore/CustomerTest.java
+++ src/test/java/com/scrumtrek/simplestore/CustomerTest.java
@@ -9,7 +9,41 @@ import static org.junit.Assert.assertEquals;
  */
 public class CustomerTest {
     @Test
-    public void shouldStatementCalculateCorrectly(){
+    public void shouldStatementCalculateOneMovieCorrectly(){
+
+        String out = "Rental record for Jake\n" +
+                "\tMovie43\t3.5\n" +
+                "Amount owed is 3.5\n"+
+                "You earned 1 frequent renter points.";
+
+        Movie movie = new Movie("Movie43", PriceCodes.Regular);
+        Rental rental = new Rental(movie, 3);
+        Customer customer = new Customer("Jake");
+        customer.addRental(rental);
+        assertEquals(customer.Statement(), out);
+    }
+
+    @Test
+    public void shouldStatementCalculateTwoMovieCorrectly(){
+
+        String out = "Rental record for Jake\n" +
+                "\tMovie43\t9.0\n" +
+                "\tAladdin\t1.5\n" +
+                "Amount owed is 10.5\n"+
+                "You earned 3 frequent renter points.";
+
+        Movie movie = new Movie("Movie43", PriceCodes.NewRelease);
+        Movie movie2 = new Movie("Aladdin", PriceCodes.Childrens);
+        Rental rental = new Rental(movie, 3);
+        Rental rental2 = new Rental(movie2,4);
+        Customer customer = new Customer("Jake");
+        customer.addRental(rental);
+        customer.addRental(rental2);
+        assertEquals(customer.Statement(), out);
+    }
+
+    @Test
+    public void shouldStatementCalculateRegularyMovieCorrectly(){
 
         String out = "Rental record for Jake\n" +
                 "\tMovie43\t2.0\n" +
